0949367a2aee4c0762bb6be67d597d88222ad632
Gerard Arnaiz Sanchez <astor98gas@proton.me>
2025-05-20 11:09:40 +0200

    fix: Correct CSS property for line clamping in SellerProfileCard component

--- FrontEnd/rema-partners/src/components/profile/SellerProfileCard.vue
+++ FrontEnd/rema-partners/src/components/profile/SellerProfileCard.vue
@@ -150,6 +150,7 @@ export default defineComponent({
 .line-clamp-2 {
     display: -webkit-box;
     -webkit-line-clamp: 2;
+    line-clamp: 2;
     -webkit-box-orient: vertical;
     overflow: hidden;
 }
